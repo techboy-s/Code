e9ad36e86e6b623f0b628c2d66a405681dbb62f4
techboy-s <65527961+techboy-s@users.noreply.github.com>
2022-10-02 15:01:34 +0530

    Time: 62 ms (82.69%), Space: 31.4 MB (61.05%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            int element = nums[i];
+           
+            
+            if(element >= 1 && element <= n){
+                 int chair = element -1;
+                if(nums[chair] != element){
+                   swap(nums[chair], nums[i]);
+                    i--;
+                }
+
+            }
+        }
+        
+        for(int i = 0;i <nums.size(); i++){
+            if(i+1 != nums[i]) return i+1;
+        }
+        
+        return n+1;
+    }
+};
